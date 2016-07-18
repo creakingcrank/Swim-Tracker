c967f27da2d92aa14767bcc54adc561f51f6256b
creakingcrank <jonathan@lightcycles.co.uk>
2016-07-18 22:31:20 +0100

    Dump_data_to_app_log on click
    3 second press of select button

--- src/comms.c
+++ src/comms.c
@@ -8,10 +8,11 @@ void dump_data_to_app_log(void) {
   int i;
   int j;
   int current_interval = get_current_interval();
-  
+  int zero_time = get_length_start_time(1);
+  APP_LOG(APP_LOG_LEVEL_INFO,", Length, Interval, Start, End, Strokes");
   for (i = 1; i<=current_interval; i++) {
     for (j = get_interval_first_length(i); j <= get_interval_last_length(i); j++) {
-      APP_LOG(APP_LOG_LEVEL_INFO,",%d,%d,%d,%d", j,i,(int)get_length_start_time(j),(int)get_length_end_time(j));
+      APP_LOG(APP_LOG_LEVEL_INFO,",%d,%d,%d,%d,%d", j,i,(int)get_length_start_time(j)-zero_time,(int)get_length_end_time(j)-zero_time,get_length_strokes(j));
     }
   }
   
--- src/main_window.c
+++ src/main_window.c
@@ -450,10 +450,15 @@ static void down_click_handler(ClickRecognizerRef recognizer, void *context) { /
   text_layer_set_text(pool_layer,pool_text_to_display);
 }
 
+static void long_select_click_handler(ClickRecognizerRef recognizer, void *context) {
+  dump_data_to_app_log();
+}
+
 static void click_config_provider(void *context) {
   window_single_click_subscribe(BUTTON_ID_SELECT, select_click_handler);
   window_single_click_subscribe(BUTTON_ID_UP, up_click_handler);
   window_single_click_subscribe(BUTTON_ID_DOWN, down_click_handler);
+  window_long_click_subscribe(BUTTON_ID_SELECT, 3000, NULL, long_select_click_handler);
 }
 static void handle_window_unload(Window* window) {
   destroy_ui();
